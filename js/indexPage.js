37f6746ca0988eae487d6f91a9bf005b45de0246
tektechnologies <info@tektechnologies.com>
2018-06-27 13:21:27 -0500

    update three files and image

--- js/indexPage.js
+++ js/indexPage.js
@@ -1,18 +1,7 @@
 /**Authors: Craig Barkley , Ben Van Meter*/
-/* Globals Papa Chart */
+/* globals Chart, , loadDistricts*/
 'use strict';
 
-// Load list of states into <select id="states">
-// function loadDistricts(districts) {
-//     var districtsEl = document.getElementById('districts');
-//     for(var i = 0; i < districts.length; i++) {
-//       var district = districts[i];
-  
-//       var optionEl = document.createElement('option');
-//       optionEl.value = district['% Teaches CS'];
-//       optionEl.innerText = district['Schools in Dist'];
-//       districtsEl.appendChild(optionEl);
-//     }
 
 
 ///////////////////////////////////////////////////////////
@@ -20,85 +9,50 @@
 //need to know if they responded or not. this is found in column ----Responding %-----
 //0% is not submitted and any column with a value has submitted.
 ///////////////////////////////////////////////////////////
-
-  function loadDistricts(districts){
-    var canvas = document.getElementById('resultsCanvas');
+var respondedYes = 0;
+var respondedNo = 0;
+function loadDistricts(districts){
+  // console.log(districts);
   
-    var response = [];
 
-    for(var i = 0; i < districts.length; i++){
-        var district = districts[i];
-                            district['Responding % '];
-                            district['% Teaches CS'];
-
-  
+  for(var i = 0; i < districts.length; i++){
+    var districtResponses = districts[i]['Responding %'];
+    //console.log(districtResponses);
+    if(districtResponses !== '0%'){
+      respondedYes++;
+    }else if (districtResponses === '0%'){
+      respondedNo++;
     }
 
+  }
+  console.log({ respondedNo, respondedYes });
+}
 
 
 
 
-
-
-// And for a doughnut chart
-var myDoughnutChart = new Chart(ctx, {
-    type: 'doughnut',
-    data: data,
-    options: options
+new Chart(document.getElementById('doughnut-chartOne'), {
+  type: 'doughnut',
+  data: {
+    labels: ['Have Responded', 'Have not responded'],
+    datasets: [
+      {
+        label: 'Population (millions)',
+        backgroundColor: ['#56020e', '#f39030'],
+        data: [loadDistricts.respondedYes, loadDistricts.respondedNo]
+      }
+    ]
+  },
+  options: {
+    title: {
+      display: true,
+      text: 'Percentage of Districts that have update CS Course'
+    }
+  }
 });
 
-data = {
-    datasets: [{
-        data: [10, 20, 30]
-    }],
-
-    // These labels appear in the legend and in the tooltips when hovering different arcs
-    labels: [
-        'Red',
-        'Yellow',
-        'Blue'
-    ]
-};
-
 //////////////////////////////////////////////////////////////////////////////////////////
-//Pie chart for those who have answered what percent is teaching and what percent is not. 
+//Pie chart for those who have answered what percent is teaching and what percent is not.
 //of those that did submit ---------- Responding % --- === true.
 //what percent of those are teaching - % Teaches CS ----===true.
-//////////////////////////////////////////////////////////////////////////////////////////
-
-function showPieChart(){
-    var canvas = document.getElementById('resultsCanvas');
-  
-  
-    for(var i = 0; i < districts.length; i++){
-      labels[i] = Placeholder.all[i].name;
-      voteCounts[i] = Placeholder.all[i].voteCount;
-      showCounts[i] = Placeholder.all[i].showCount;
-  
-    }
-
-
-
-
-
-
-
-// And for a doughnut chart
-var myDoughnutChart = new Chart(ctx, {
-    type: 'doughnut',
-    data: data,
-    options: options
-});
-
-data = {
-    datasets: [{
-        data: [10, 20, 30]
-    }],
-
-    // These labels appear in the legend and in the tooltips when hovering different arcs
-    labels: [
-        'Red',
-        'Yellow',
-        'Blue'
-    ]
-};
+/////////////////////////////////////////////////////////////////////////////////////////
