b2f69e511edf78e3a217b53240de92a90a10b373
tektechnologies <info@tektechnologies.com>
2018-06-29 10:31:50 -0500

    fridayMorningWork

--- js/importData.js
+++ js/importData.js
@@ -1,33 +1,5 @@
-/* globals Papa,  loadDistricts, loadCsDistricts, loadGradeCSData*/
+/* globals Papa, loadGradeCSData*/
 
-
-
-// Load data on window load
-// function loadCsData() {
-
-//   var csvUrl = 'data/DataBySchoolDistrict.csv';
-//   Papa.parse(csvUrl, {
-
-//     // Download CSV data from csvUrl
-//     download: true,
-
-//     // Use CSV header row
-//     header: true,
-
-//     // When CSV data are available...
-//     complete: function(results) {
-//       //console.log('CSV loaded:', results.data);
-
-//       // results.data is an array of objects
-//       // object keys are from CSV header row
-//       loadDistricts(results.data);
-//       loadCsDistricts(results.data);
-//     }
-//   });
-
-// }
-
-// window.addEventListener('load', loadCsData);
 
 
 
